eac70fdb46fb48000d31745c91f96383678c1d3d
wesleyfn <wesleyfnpa@gmail.com>
2023-06-18 00:51:47 -0300

    novo relatorio

---
+++ code/relatorio3/euler/euler.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import sympy as sp
+
+# Funções para entrada e saída de dados
+def read_file(file_name: str):
+    path_file = os.path.abspath(os.path.join(os.getcwd(), file_name))
+    with open(path_file, 'r') as f:
+        try:
+            expr = sp.parse_expr(f.readline())
+            x0 = float(f.readline())
+            y0 = float(f.readline())
+            h = float(f.readline())
+            n = int(f.readline())
+        except (ValueError, TypeError):
+            return None, 0.0, 0.0, 0.0, 0
+
+    return expr, x0, y0, h, n
+            
+def save_results(file_name: str, result: str):
+    with open(file_name, 'a+') as f:
+        f.truncate(0)
+        f.write(result)
+        
+def euler_method(expr, x0, y0, h, n_x):
+    list_x = np.linspace(x0, x0 + n_x*h, n_x+1)
+    list_y = np.zeros(n_x+1)
+    list_y[0] = y0
+    
+    f = lambda x, y: sp.N(expr.subs({'x': x, 'y': y})) if hasattr(expr, 'subs') else expr
+    for i in range(n_x):
+        list_y[i+1] = list_y[i] + h * f(list_x[i], list_y[i])
+
+    return list(list_x), list(list_y)
+
+def run():
+    FILE_NAME = 'input.txt'
+    expr, x0, y0, h, n = read_file(FILE_NAME)
+    
+    if expr is not None:
+        list_x, list_y = euler_method(expr, x0, y0, h, n)
+        save_results('output.txt', f'euler_method: x={list_x}, y={list_y}')
+
+# Chama a função principal
+if __name__ == '__main__':
+    run()
---
+++ code/relatorio3/ralston/ralston.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import sympy as sp
+
+# Funções para entrada e saída de dados
+def read_file(file_name: str):
+    path_file = os.path.abspath(os.path.join(os.getcwd(), file_name))
+    with open(path_file, 'r') as f:
+        try:
+            expr = sp.parse_expr(f.readline())
+            x0 = float(f.readline())
+            y0 = float(f.readline())
+            h = float(f.readline())
+            n_x = int(f.readline())
+        except (ValueError, TypeError):
+            return None, 0.0, 0.0, 0.0, 0, 0
+
+    return expr, x0, y0, h, n_x
+            
+def save_results(file_name: str, result: str):
+    with open(file_name, 'a+') as f:
+        f.truncate(0)
+        f.write(result)
+        
+def ralston_method(expr, x0, y0, h, n_x):
+    list_x = np.linspace(x0, x0 + n_x * h, n_x+1)
+    list_y = np.zeros(n_x+1)
+    list_y[0] = y0
+    
+    f = lambda x, y: sp.N(expr.subs({'x': x, 'y': y})) if hasattr(expr, 'subs') else expr
+    for i in range(n_x):
+        k1 = f(list_x[i], list_y[i])
+        k2 = f(list_x[i] + (3*h)/4, list_y[i] + (3*h*k1)/4)
+        list_y[i+1] = list_y[i] + h * ((k1/3)+ (2*k2)/3)
+
+    return list(list_x), list(list_y)
+
+def run():
+    FILE_NAME = 'input.txt'
+    expr, x0, y0, h, n = read_file(FILE_NAME)
+    
+    #if expr is not None:
+    list_x, list_y = ralston_method(expr, x0, y0, h, n)
+    save_results('output.txt', f'ralston_method: x={list_x}, y={list_y}')
+
+# Chama a função principal
+if __name__ == '__main__':
+    run()
